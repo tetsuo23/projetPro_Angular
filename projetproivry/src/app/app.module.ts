4c5ce57681c8c8cc95e110f09d69aca631f6fcc3
tetsuo23 <carretmo@gmail.com>
2020-05-15 17:54:09 +0200

    menu burger + navigation

--- projetproivry/src/app/app.module.ts
+++ projetproivry/src/app/app.module.ts
@@ -7,18 +7,33 @@ import { LandingComponent } from './landing/landing.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatVideoModule } from 'mat-video';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { AccueilComponent } from './accueil/accueil.component';
+import { HeaderComponent } from './navigation/header/header.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
-    AccueilComponent
+    AccueilComponent,
+    HeaderComponent,
+    SidenavListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatVideoModule
+    MatVideoModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
